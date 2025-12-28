ce8f1b8e2039b0a5e19a46d1ecace3c6a12660d9
Matzkowsky <matzkowsky@arcor.de>
2025-12-28 08:58:29 +0100

    Auto sniping on mouse control layer (1)

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/neo/keymap.c
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/neo/keymap.c
@@ -26,7 +26,7 @@ enum dilemma_keymap_layers {
 };
 
 // Automatically enable sniping-mode on the pointer layer.
-// #define DILEMMA_AUTO_SNIPING_ON_LAYER LAYER_POINTER
+#define DILEMMA_AUTO_SNIPING_ON_LAYER LAYER_LOWER // mouse control layer
 
 #define LOWER MO(LAYER_LOWER)
 #define RAISE MO(LAYER_RAISE)
