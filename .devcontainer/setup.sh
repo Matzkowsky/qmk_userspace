f83b9e036095710bfcff83c93de3ffc0475fc9c9
Matzkowsky <matzkowsky@arcor.de>
2026-05-02 15:33:20 +0200

    Changed bastard-qmk branch to bkb-procyon (fix pointing device compile error)

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -14,7 +14,7 @@ userspacePath="$1"
 git config --global --add safe.directory "$userspacePath"
 git submodule update --init --recursive
 
-[ -d /workspaces/bastardkb-qmk ] || git clone -b bkb-develop https://github.com/Bastardkb/bastardkb-qmk.git /workspaces/bastardkb-qmk
+[ -d /workspaces/bastardkb-qmk ] || git clone -b bkb-procyon https://github.com/Bastardkb/bastardkb-qmk.git /workspaces/bastardkb-qmk
 git config --global --add safe.directory /workspaces/bastardkb-qmk
 
 qmk config user.qmk_home=/workspaces/bastardkb-qmk
